994f442dbd94020f73250503dcb9e4b417e7cea3
dolf <rudolfbyker@gmail.com>
2022-11-04 11:01:31 +0200

    Format Python code using Black.

--- create_certificate.py
+++ create_certificate.py
@@ -10,8 +10,8 @@ import click
 
 
 @click.command()
-@click.argument('ca')
-@click.argument('domains', nargs=-1)
+@click.argument("ca")
+@click.argument("domains", nargs=-1)
 def main(ca, domains) -> int:
     """
     CA: The name of the CA you passed to `create_certificate_authority.py`.
@@ -28,21 +28,25 @@ def main(ca, domains) -> int:
         return 0
 
     # Use the first domain name as the folder name.
-    out_dir = Path('out', ca, domains[0])
+    out_dir = Path("out", ca, domains[0])
     out_dir.mkdir(parents=True, exist_ok=True)
 
     # Check if the certificate authority files exist.
-    ca_path = Path(out_dir, '..', 'root.pem').resolve()
+    ca_path = Path(out_dir, "..", "root.pem").resolve()
     if not ca_path.exists():
-        print(f"File {ca_path} does not exist. Did you run create_certificate_authority.py first?")
+        print(
+            f"File {ca_path} does not exist. Did you run create_certificate_authority.py first?"
+        )
         return 1
-    ca_key_path = Path(out_dir, '..', 'private.key').resolve()
+    ca_key_path = Path(out_dir, "..", "private.key").resolve()
     if not ca_key_path.exists():
-        print(f"File {ca_key_path} does not exist. Did you run create_certificate_authority.py first?")
+        print(
+            f"File {ca_key_path} does not exist. Did you run create_certificate_authority.py first?"
+        )
         return 1
 
     # Generate private key if it does not exist yet.
-    key_path = Path(out_dir, 'private.key')
+    key_path = Path(out_dir, "private.key")
     if key_path.exists():
         print(f"Private key {key_path} already exists.")
     else:
@@ -50,36 +54,42 @@ def main(ca, domains) -> int:
         Popen(f"openssl genrsa -out '{key_path}' 2048", shell=True).wait()
 
     # Create certificate-signing request if it does not exist.
-    csr_path = Path(out_dir, 'request.csr')
+    csr_path = Path(out_dir, "request.csr")
     if csr_path.exists():
         print(f"Certificate signing request {csr_path} already exists.")
     else:
         print("Creating certificate signing request…")
-        Popen(f"openssl req -new -key '{key_path}' -out '{csr_path}'", shell=True).wait()
+        Popen(
+            f"openssl req -new -key '{key_path}' -out '{csr_path}'", shell=True
+        ).wait()
 
     # Create a config file for the extensions.
     print("Creating config file for extensions…")
-    ext_path = Path(out_dir, 'config.ext')
+    ext_path = Path(out_dir, "config.ext")
     alt_names = "\n".join([f"DNS.{i + 1} = {d}" for i, d in enumerate(domains)])
     # TODO: maybe rotate the old file instead of overwriting?
-    with ext_path.open(mode='w') as f:
-        f.write(f"""\
+    with ext_path.open(mode="w") as f:
+        f.write(
+            f"""\
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 subjectAltName = @alt_names
 [alt_names]
 {alt_names}
-""")
+"""
+        )
 
     # Create the signed certificate.
     print("Creating signed certificate…")
-    cert_path = Path(out_dir, 'certificate.crt')
+    cert_path = Path(out_dir, "certificate.crt")
     # TODO: maybe rotate the old file instead of overwriting?
-    command = f"openssl x509 -req -in '{csr_path}' -CA '{ca_path}' -CAkey '{ca_key_path}' -CAcreateserial " \
-              f"-out '{cert_path}' -days 1825 -sha256 -extfile '{ext_path}'"
+    command = (
+        f"openssl x509 -req -in '{csr_path}' -CA '{ca_path}' -CAkey '{ca_key_path}' -CAcreateserial "
+        f"-out '{cert_path}' -days 1825 -sha256 -extfile '{ext_path}'"
+    )
     Popen(command, shell=True).wait()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
--- create_certificate_authority.py
+++ create_certificate_authority.py
@@ -11,17 +11,17 @@ import click
 
 
 @click.command()
-@click.argument('name')
+@click.argument("name")
 def main(name) -> None:
     """
     NAME: Any name you want to give to your new CA.
     """
     # Create folder for our CA and its certificates.
-    out_dir = Path('out', name)
+    out_dir = Path("out", name)
     out_dir.mkdir(parents=True, exist_ok=True)
 
     # Generate private key for the CA.
-    ca_key_path = Path(out_dir, 'private.key')
+    ca_key_path = Path(out_dir, "private.key")
     if ca_key_path.exists():
         print(f"Private key {ca_key_path} already exists.")
     else:
@@ -29,11 +29,11 @@ def main(name) -> None:
         Popen(f"openssl genrsa -des3 -out '{ca_key_path}' 2048", shell=True).wait()
 
     # Generate root certificate.
-    ca_root_path = Path(out_dir, 'root.pem')
+    ca_root_path = Path(out_dir, "root.pem")
     # TODO: maybe rotate the old file instead of overwriting?
     command = f"openssl req -x509 -new -nodes -key '{ca_key_path}' -sha256 -days 360 -out '{ca_root_path}'"
     Popen(command, shell=True).wait()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
